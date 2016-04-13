f4b4865205b313c2b4061da47c927cc13993d0bf
ciskeboekelo <cfboekelo@gmail.com>
2016-04-13 21:53:10 +0200

    Javadoc fix.

--- lib/src/main/java/com/orangegangsters/github/swipyrefreshlayout/library/MaterialProgressDrawable.java
+++ lib/src/main/java/com/orangegangsters/github/swipyrefreshlayout/library/MaterialProgressDrawable.java
@@ -139,6 +139,8 @@ class MaterialProgressDrawable extends Drawable implements Animatable {
      * Set the overall size for the progress spinner. This updates the radius
      * and stroke width of the ring.
      *
+     * @param size One of {@link #LARGE} or
+     *            {@link #DEFAULT}
      */
     public void updateSizes(@ProgressDrawableSize int size) {
         if (size == LARGE) {
